0989a0bc13c643528b860e0c3579dfff9701e9fb
artantstar <artantstar@gmail.com>
2021-10-04 21:21:44 +0300

    feat: complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,57 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.repeat(Math.ceil(message.length / key.length)).toUpperCase();
+
+    let firstSimbol = 65
+    let alphabetLength = 26;
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt(0) < 65 || message[i].charCodeAt(0) > 90) {
+        result.push(message[i]);
+        message = message.replace(message[i], '');
+        i--;
+      } else {
+        let letterIdx = message.charCodeAt(i) - firstSimbol;
+        let shift = key.charCodeAt(i) - firstSimbol;
+        result.push(String.fromCharCode(firstSimbol + (letterIdx + shift) % alphabetLength));
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    message = message.toUpperCase();
+    key = key.repeat(Math.ceil(message.length / key.length)).toUpperCase();
+
+    let firstSimbol = 65
+    let alphabetLength = 26;
+    let result = [];
+
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].charCodeAt(0) < 65 || message[i].charCodeAt(0) > 90) {
+        result.push(message[i]);
+        message = message.replace(message[i], '');
+        i--;
+      } else {
+        let letterIdx = message.charCodeAt(i) - firstSimbol;
+        let shift = key.charCodeAt(i) - firstSimbol;
+        result.push(String.fromCharCode(firstSimbol + (letterIdx - shift + alphabetLength) % alphabetLength));
+      }
+    }
+
+    return this.direct ? result.join('') : result.reverse().join('');
   }
 }
